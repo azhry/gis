9844c4286d5bc16b037aaa22b00e52ffbc50932f
azhry <arliansyah_azhary@yahoo.com>
2018-03-04 19:51:41 +0700

    Laporan jumlah proyek

--- application/views/kepala_satuan_kerja/grafik.php
+++ application/views/kepala_satuan_kerja/grafik.php
@@ -13,6 +13,7 @@
             <header class="panel-heading">
                 <div class="panel-actions"></div>
                 <h3 class="panel-title">Grafik Keseluruhan Proyek</h3>
+                <button onclick="downloadPDF();" id="unduh-laporan" type="button" class="btn btn-primary"><i class="fa fa-download"></i> Unduh Grafik Laporan</button>
             </header>
             <div class="panel-body">
             	<div class="row">
@@ -29,8 +30,9 @@
 </div>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
-
-<!-- <script type="text/javascript" src="<?= base_url( 'assets/vendor/chartJs/Chart.min.js' ) ?>"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.debug.js"></script>
+<script type="text/javascript" src="<?= base_url( 'assets/js/html2canvas.js' ) ?>"></script>
+<!-- <script type="text/javascript" src="<?= base_url( 'assets/vendor/chart-js/Chart.min.js' ) ?>"></script> -->
 <script type="text/javascript">
 	$( document ).ready(function() {
 
@@ -54,33 +56,47 @@
             data: data
 		});
 
-		new Chart(document.getElementById("laporan-keseluruhan-proyek"), {
-	    type: 'bar',
-	    data: {
-	      labels: labels,
-	      datasets: datasets
-	    },
-	    options: {
-	      legend: { display: false },
-	      title: {
-	        display: true,
-	        text: 'Grafik Jumlah Projek Setiap Kabupaten'
-	      },
-	      scales: {
-	      	yAxes: [{
-	      		ticks: {
-	      			min: 0,
-	      			max: <?= count( $proyek ) ?>,
-	      			stepSize: 1
-	      		}
-	      	}]
-	      }
-	    }
-	});
+		var ctx = new Chart(document.getElementById("laporan-keseluruhan-proyek"), {
+		    type: 'bar',
+		    data: {
+		      labels: labels,
+		      datasets: datasets
+		    },
+		    options: {
+		      legend: { display: false },
+		      title: {
+		        display: true,
+		        text: 'Grafik Jumlah Proyek Setiap Kabupaten'
+		      },
+		      scales: {
+		      	yAxes: [{
+		      		ticks: {
+		      			min: 0,
+		      			max: <?= count( $proyek ) ?>,
+		      			stepSize: 1
+		      		}
+		      	}]
+		      }
+		    }
+		});
 
 	});
 
 	function getRandom(max) {
 	  	return Math.random() * Math.floor(max);
 	}
+
+	function downloadPDF() {
+		html2canvas(document.getElementById("laporan-keseluruhan-proyek"), {
+            useCORS: true,
+            onrendered: function( canvas ) {
+
+                var imgData = canvas.toDataURL( 'image/png' );
+                var doc = new jsPDF( 'p', 'mm' );
+                doc.addImage( imgData, 'PNG', 3, 3 );
+                doc.save( 'laporan-jumlah-proyek.pdf' );
+
+            }
+        });
+	 }
 </script>
