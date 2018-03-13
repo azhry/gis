7bcdb5434892e368c7662f437346b51690c6bf61
azhry <arliansyah_azhary@yahoo.com>
2018-03-13 12:22:38 +0700

    test error exception view

--- application/views/errors/html/error_exception.php
+++ application/views/errors/html/error_exception.php
@@ -5,7 +5,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
 
 <h4>An uncaught Exception was encountered</h4>
-
 <p>Type: <?php echo get_class($exception); ?></p>
 <p>Message: <?php echo $message; ?></p>
 <p>Filename: <?php echo $exception->getFile(); ?></p>
